31987cbf3408740f23867004ca0cd155ff0dc40e
xAksssenov <aks.kirill32@gmail.com>
2025-04-11 19:10:29 +0300

    feat: change db on postres

--- veerrzastore/veerrzastore/settings.py
+++ veerrzastore/veerrzastore/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,10 +84,10 @@ WSGI_APPLICATION = 'veerrzastore.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL', 'postgres://veerrza_user:veerrza_password@db:5432/veerrza_db'),
+        conn_max_age=600
+    )
 }
 
 # Password validation
