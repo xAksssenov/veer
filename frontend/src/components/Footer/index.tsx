95dd239ea9048b28c5ed6665abb4a70dd0f8cd48
xAksssenov <aks.kirill32@gmail.com>
2025-05-18 17:51:34 +0300

    feat: add official documents on page in footer

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -2,6 +2,8 @@ import { FaTelegram } from "react-icons/fa";
 import avitoIcon from "../../assets/svg/avito.svg";
 import { ReactSVG } from "react-svg";
 import styles from "./index.module.scss";
+import preOrderOffer from "../../assets/files/Oferta_prezakaza_VEERRZA.pdf";
+import publicOffer from "../../assets/files/Publichnaya_oferta_VEERRZA.pdf";
 
 const Footer = () => {
   return (
@@ -11,14 +13,34 @@ const Footer = () => {
         <div className={styles.sellersInfo}>
           <p>Верещагин Михаил Васильевич - ИНН: 301611373729</p>
           <p>Рзазаде Магаммед Вугар оглы - ИНН: 301515975576</p>
-          <a
-            href="https://t.me/mveerm"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.sellersInfo__link}
-          >
-            У вас возникли вопросы?
-          </a>
+          <div className={styles.sellersInfo__links}>
+            <a
+              href="https://t.me/mveerm"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.sellersInfo__link}
+            >
+              У вас возникли вопросы?
+            </a>
+            <a
+              href={preOrderOffer}
+              target="_blank"
+              rel="noopener noreferrer"
+              download
+              className={styles.sellersInfo__link}
+            >
+              Оферта предзаказа VEERRZA
+            </a>
+            <a
+              href={publicOffer}
+              target="_blank"
+              rel="noopener noreferrer"
+              download
+              className={styles.sellersInfo__link}
+            >
+              Публичная оферта VEERRZA
+            </a>
+          </div>
         </div>
         <div className={styles.socialLinks}>
           <div className={styles.socialItem}>
