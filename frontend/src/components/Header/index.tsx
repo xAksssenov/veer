32a35715326c7bc48b8de88ea7880280eb34dd64
xAksssenov <aks.kirill32@gmail.com>
2025-06-06 14:50:16 +0300

    fix: logo and styles

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,22 +1,43 @@
 import { Link } from "react-router";
 import cartIcon from "../../assets/svg/cart.svg";
-import logoIcon from "/assets/img/logo_1.png";
+import logoSvg from "../../assets/svg/logo.svg";
+import logoPng from "/assets/img/logo_1.png";
 import { ReactSVG } from "react-svg";
 import styles from "./index.module.scss";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Header = () => {
   const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+  const [isMobile, setIsMobile] = useState(false);
 
   const togglePopup = () => {
     setIsPopupOpen(!isPopupOpen);
   };
 
+  useEffect(() => {
+    const checkMobile = () => {
+      setIsMobile(window.innerWidth < 900);
+    };
+
+    checkMobile();
+
+    window.addEventListener("resize", checkMobile);
+
+    return () => {
+      window.removeEventListener("resize", checkMobile);
+    };
+  }, []);
+
   return (
     <div className={styles.header}>
       <div className={styles.header__container}>
         <Link to="/" className={styles.header__logo}>
-          <img src={logoIcon} className={styles.header__icon} />
+          {isMobile ? (
+            <ReactSVG src={logoSvg} className={styles.header__icon} />
+          ) : (
+            <img src={logoPng} className={styles.header__icon} alt="logo" />
+          )}
         </Link>
 
         <div className={styles.header__nav}>
